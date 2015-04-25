e6fc0d600f29f3e696ae48c3824fb77f08eb2e35
Yan Wang <yan_wang10@126.com>
2015-04-25 17:01:23 -0400

    add merge arff

---
+++ src/merge_arff.py
@@ -0,0 +1,47 @@
+from extract_features import arff_dump
+import sys
+
+def arff_load(filename):
+	content = open(filename, 'r').readlines()
+	feature_list = []
+	for line in content:
+		if len(line) < 2:
+			continue
+		if line.startswith('@'):
+			line = line.lower()
+			seg = line.strip().split(' ')
+			if line.startswith('@attribute'):
+				feature_list.append([seg[1], ' '.join(seg[2:]), []])
+		else:
+			print seg
+			seg = line.strip().split(',')
+			print len(seg), len(feature_list)
+			assert len(seg) == len(feature_list)
+			for i, s in enumerate(seg):
+				if 'numeric' == feature_list[i][1]:
+					feature_list[i][2].append(float(s))
+				else:
+					feature_list[i][2].append(s)
+			continue
+	fd = dict()
+	for f in feature_list:
+		fd[f[0]] = ({'name':f[0], 'type':f[1]}, f[2])
+	return fd
+
+def merge_dict(dst, src):
+	for key, value in src.items():
+		if not key in dst:
+			dst[key] = value
+	return dst
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print 'Usage: <dst> <src1> <src2>...'
+		exit(0)
+	dst = sys.argv[1]
+	data = {}
+	for i in xrange(2, len(sys.argv)):
+		data = merge_dict(data, arff_load(sys.argv[i]))
+	real_data = [value for value in data.values()]
+	arff_dump(dst, real_data)
+
