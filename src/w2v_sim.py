c2fa5cc3b8d4f8a21128b70d9337336851bd84b0
Yan Wang <yan_wang10@126.com>
2015-04-26 18:14:22 -0400

    content words

--- src/w2v_sim.py
+++ src/w2v_sim.py
@@ -1,4 +1,4 @@
-
+from pattern.en import tag
 w2vfile = '../data/LM-train-50.txt'
 
 #@profile
@@ -10,8 +10,17 @@ def inner_product(a, b):
 		s += ia * ib
 	return s
 
+def average(l):
+	return sum(l) / len(l)
+
+accept_set = set(['JJ', 'VB', 'NN', 'RB'])
+def accept(w, t):
+	return t[:2] in accept_set
+
+oov = set()
+
 #@profile
-def w2v_sim(text):
+def w2v_sim(text, pos = None):
 	vocab = dict()
 	for article in text:
 		for sentence in article:
@@ -32,10 +41,26 @@ def w2v_sim(text):
 	print count, '/', len(vocab)
 	avg_sim = list()
 	max_sim = list()
+	cont_sim_avg = list()
+	cont_sim_var = list()
+	top_sim_avg = list()
+	bot_sim_avg = list()
+	dif_sim = list()
 	for r, article in enumerate(text):
 		art_max = 0.0
 		art_avg = 0.0
+		cont_links = list()
 		for sen in article:
+			token_tags = tag(' '.join(sen[1:-1]))
+			cont_tokens = list()
+			for w, t in token_tags:
+				if accept(w, t) and w in vocab:
+					cont_tokens.append(w)
+			for i in xrange(len(cont_tokens)):
+				for j in xrange(i + 1, len(cont_tokens)):
+					p = inner_product(vocab[cont_tokens[i]], vocab[cont_tokens[j]])
+					cont_links.append(p)
+			'''
 			pair = list()
 			for i in xrange(len(sen)):
 				for j in xrange(i + 1, len(sen)):
@@ -43,12 +68,27 @@ def w2v_sim(text):
 					pair.append(p)
 			art_max += max(pair)
 			art_avg += sum(pair) / len(pair)
-
-		max_sim.append(art_max / len(article))
-		avg_sim.append(art_avg / len(article))
+			'''
+		#max_sim.append(art_max / len(article))
+		#avg_sim.append(art_avg / len(article))
 		if r % 10 == 0:
 			print r
-			
+		
+		s = average(cont_links)
+		cont_sim_avg.append(s)
+		top = 5
+		if len(cont_links) < top:
+			top = len(cont_links)
+		cont_links = map(lambda a: (a - s) ** 2, cont_links)
+		dif_sim.append(average(cont_links[:top]) - average(cont_links[-top:]))
+
+	'''
 	return [({'name':'w2v_avg_sim', 'type':'numeric'}, avg_sim), \
 			({'name':'w2v_max_sim', 'type':'numeric'}, max_sim)]
 	
+	'''
+	return [({'name':'w2v_cont_avg', 'type':'numeric'}, cont_sim_avg), \
+			#({'name':'w2v_cont_var', 'type':'numeric'}, cont_sim_var), \
+			#({'name':'w2v_top_avg', 'type':'numeric'}, top_sim_avg), \
+			#({'name':'w2v_bot_avg', 'type':'numeric'}, bot_sim_avg)]
+			({'name':'w2v_cont_dif', 'type':'numeric'}, dif_sim)]
