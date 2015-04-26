64cabe4be02c5bd53681da2745f74f1c71c10689
Yan Wang <yan_wang10@126.com>
2015-04-26 18:15:02 -0400

    fix bug in merge arff

--- src/merge_arff.py
+++ src/merge_arff.py
@@ -12,10 +12,9 @@ def arff_load(filename):
 			seg = line.strip().split(' ')
 			if line.startswith('@attribute'):
 				feature_list.append([seg[1], ' '.join(seg[2:]), []])
+				print feature_list[-1]
 		else:
-			print seg
 			seg = line.strip().split(',')
-			print len(seg), len(feature_list)
 			assert len(seg) == len(feature_list)
 			for i, s in enumerate(seg):
 				if 'numeric' == feature_list[i][1]:
@@ -35,13 +34,19 @@ def merge_dict(dst, src):
 	return dst
 
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
+	if len(sys.argv) < 2:
 		print 'Usage: <dst> <src1> <src2>...'
 		exit(0)
 	dst = sys.argv[1]
 	data = {}
+	real_data = []
 	for i in xrange(2, len(sys.argv)):
 		data = merge_dict(data, arff_load(sys.argv[i]))
-	real_data = [value for value in data.values()]
+	LABEL = 'label'
+	if LABEL in data:
+	 	real_data.append(data[LABEL])
+	for key, value in data.items():
+		if key != LABEL:
+			real_data.append(value)
 	arff_dump(dst, real_data)
 
