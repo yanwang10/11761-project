6c3365b1f31f83def879c31360d7b237006dfff7
Yan Wang <yan_wang10@126.com>
2015-04-26 22:35:33 -0400

    final feature

--- src/w2v_sim.py
+++ src/w2v_sim.py
@@ -1,5 +1,5 @@
 
-w2vfile = '../data/LM-train-50.txt'
+w2vfile = './models/LM-train-50.txt'
 
 #@profile
 def inner_product(a, b):
@@ -29,7 +29,7 @@ def w2v_sim(text):
 		if seg[0] in vocab:
 			count += 1
 			vocab[seg[0]] = [float(seg[j]) for j in xrange(1, dim + 1)]
-	print count, '/', len(vocab)
+
 	avg_sim = list()
 	max_sim = list()
 	for r, article in enumerate(text):
@@ -46,9 +46,6 @@ def w2v_sim(text):
 
 		max_sim.append(art_max / len(article))
 		avg_sim.append(art_avg / len(article))
-		if r % 10 == 0:
-			print r
-			
+
 	return [({'name':'w2v_avg_sim', 'type':'numeric'}, avg_sim), \
 			({'name':'w2v_max_sim', 'type':'numeric'}, max_sim)]
-	
